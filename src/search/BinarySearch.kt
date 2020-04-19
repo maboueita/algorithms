7349c3f32a0287c89e485e08ec0a7b01cd670ce3
maboueita <mahmoud.aboueita@gmail.com>
2020-04-19 10:57:19 +0200

    initial commit

---
+++ src/search/BinarySearch.kt
@@ -0,0 +1,60 @@
+package search
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class BinarySearch {
+
+    fun binarySearch(numbers: List<Int>, target: Int): Int {
+        var min = 0
+        var max = numbers.size - 1
+        while (min <= max) {
+            val mid = (min + max) / 2
+            when {
+                numbers[mid] == target -> {
+                    return mid
+                }
+                target > numbers[mid] -> {
+                    min = mid + 1
+                }
+                else -> {
+                    max = mid - 1
+                }
+            }
+        }
+        return -1
+    }
+
+    @Test
+    fun `binary search finds correct index of a target when found or -1 otherwise`() {
+
+        
+        val numbersList = listOf(
+            63, 65, 67, 69, 71, 73, 75,
+            79, 81, 83, 85, 87, 89,
+            91, 93, 95
+        )
+
+        // Existing targets
+        val indexOf93 = binarySearch(numbersList, 93)
+        assertThat(indexOf93, equalTo(14))
+
+        val indexOf91 = binarySearch(numbersList, 91)
+        assertThat(indexOf91, equalTo(13))
+
+        val indexOf79 = binarySearch(numbersList, 79)
+        assertThat(indexOf79, equalTo(7))
+
+        val indexOf81 = binarySearch(numbersList, 81)
+        assertThat(indexOf81, equalTo(8))
+
+        //Non-existing targets
+        val indexOf0 = binarySearch(numbersList, 0)
+        assertThat(indexOf0, equalTo(-1))
+
+        val indexOf120 = binarySearch(numbersList, 120)
+        assertThat(indexOf120, equalTo(-1))
+
+    }
+}
---
+++ src/sort/SelectionSort.kt
@@ -0,0 +1,39 @@
+package sort
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class SelectionSort {
+    
+    fun selectionSort(numbers: MutableList<Int>): MutableList<Int> {
+        for (i in 0 until numbers.size) {
+            var min = numbers[i]
+            for (j in i + 1 until numbers.size) {
+                if (numbers[j] < min) {
+                    min = numbers[j]
+                    val temp = numbers[i]
+                    numbers[i] = numbers[j]
+                    numbers[j] = temp
+                }
+            }
+        }
+        return numbers
+    }
+
+
+    @Test
+    fun `test output of different sizes against standard sort implementation`() {
+
+        val unsortedList = mutableListOf(3, 8, 7, 9, 2, 6, 5)
+        val standardSortedList: MutableList<Int> = unsortedList.sorted().toMutableList()
+        val selectionSortedSort: MutableList<Int> = selectionSort(unsortedList)
+        assertThat(selectionSortedSort, equalTo(standardSortedList))
+
+        val unsortedList2 = List(1000) { i -> i + 1 }.shuffled().toMutableList()
+        val standardSortedList2 = unsortedList2.sorted().toMutableList()
+        val selectionSortedList2 = selectionSort(unsortedList2)
+        assertThat(selectionSortedList2, equalTo(standardSortedList2))
+    }
+
+}
